daf51fe4d50f489a517e0925f46fa26830c402d9
preetisi <singhpreeti236@gmail.com>
2016-03-07 15:57:33 -0500

    adding certs to gitignore, modified cert path in env.sh

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 export DOCKER_HOST=tcp://dblof.broadinstitute.org:2376
-export DOCKER_CERT_PATH="$(cd $(dirname $BASH_SOURCE); pwd)/docker"
+export DOCKER_CERT_PATH="$(cd $(dirname $BASH_SOURCE); pwd)/docker/certs"
 export DOCKER_TLS_VERIFY=1
