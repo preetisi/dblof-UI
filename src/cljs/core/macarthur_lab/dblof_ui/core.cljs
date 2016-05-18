c9dfa378c91c35f0aced89804882dca920aaa227
preetisi <singhpreeti236@gmail.com>
2016-05-18 01:18:39 -0400

    Revert "added header row to variant table"
    This reverts commit 3ca6bec5920ac76b949cafcb1e87ca4066454c99.

--- src/cljs/core/macarthur_lab/dblof_ui/core.cljs
+++ src/cljs/core/macarthur_lab/dblof_ui/core.cljs
@@ -137,21 +137,8 @@
       [:div {:ref "plot2" :style {:width 600 :height 300}}]
       [:div {:ref "plot3" :style {:width 600 :height 300}}]
 
-      [:div {:style {:padding "50px" :color "black"}}
-      [:div {:style {:display "flex"}}
-       [:div {:style {:flex "0 0 20%" :padding "10px"
-                      :overflow "hidden" :textOverflow "ellipsis"}}
-         "variant_id"]
-       [:div {:style {:flex "0 0 20%" :padding "10px"}}
-            "chrom"]
-       [:div {:style {:flex "0 0 20%"  :padding "10px"}}
-              "pos"]
-       [:div {:style {:flex "0 0 20%" :padding "10px"
-                      :overflow "hidden" :textOverflow "ellipsis"}}
-            "allele_freq"]
-      [:div {:style {:flex "0 0 20%" :padding "10px"}}
-
-            "hom_count"]]
+      [:div {:style {:padding "50px" :color "#FF0000"}}
+        "Variants"
         [:div {} (map (fn [x]
           [:div {:style {:display "flex"}}
            [:div {:style {:flex "0 0 20%" :padding "10px"
@@ -161,11 +148,9 @@
                   (get x "chrom")]
            [:div {:style {:flex "0 0 20%"  :padding "10px"}}
                   (get x "pos")]
-           [:div {:style {:flex "0 0 20%" :padding "10px"
-                          :overflow "hidden" :textOverflow "ellipsis"}}
+           [:div {:style {:flex "0 0 20%" :padding "10px"}}
                   (get x "allele_freq")]
           [:div {:style {:flex "0 0 20%" :padding "10px"}}
-
                   (get x "hom_count")]
           ]) (:variants @state))]]
       [:div {}
