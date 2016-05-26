ceee2a9e3db198b6a32f7b392c7b372028f0773e
David Mohs <dgit@mohsinc.com>
2016-05-26 11:10:54 -0400

    Load gene exons from new data

--- src/cljs/core/macarthur_lab/dblof_ui/pd.cljs
+++ src/cljs/core/macarthur_lab/dblof_ui/pd.cljs
@@ -31,16 +31,18 @@
    (fn [{:keys [this props state next-props]}]
      (when-not (apply = (map :gene-name [props next-props]))
        (swap! state dissoc :status)
-       (this :load-exon-data)))
-   :component-did-mount (fn [{:keys [this]}] (this :load-exon-data))
-   :load-exon-data
-   (fn [{:keys [props state]}]
+       (this :load-data (:gene-name next-props))))
+   :component-did-mount
+   (fn [{:keys [this props]}] (this :load-data (:gene-name props)))
+   :load-data
+   (fn [{:keys [props state]} gene-name]
      (u/ajax {:url (str (:api-url-root props) "/exec-sql")
               :method :post
               :data (u/->json-string
-                     {:sql (str "select exonId `id`, exonStart `start`, size from gene_exon\n"
-                                "where symbol=?")
-                      :params [(clojure.string/upper-case (:gene-name props))]})
+                     {:sql (str "select start, stop-start size from gene_exons_v2 e\n"
+                                "inner join gene_symbols s on e.gene_id=s.gene_id\n"
+                                "where s.symbol=?")
+                      :params [(clojure.string/upper-case gene-name)]})
               :on-done (fn [{:keys [get-parsed-response]}]
                          (swap! state assoc :status
                                 {:code :loaded :data (get (get-parsed-response) "rows")}))}))})
